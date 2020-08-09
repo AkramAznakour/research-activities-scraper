24d0590017517164a81f39d7b926eff7d8c1518b
Akram Aznakour <35656299+AkramAznakour@users.noreply.github.com>
2020-08-09 12:21:43 +0100

    Setup puppeteer-extra-plugin-stealth

--- app/scraper/helper/setupBrowserPage.js
+++ app/scraper/helper/setupBrowserPage.js
@@ -1,4 +1,7 @@
-const puppeteer = require("puppeteer");
+const puppeteer = require("puppeteer-extra");
+const StealthPlugin = require("puppeteer-extra-plugin-stealth");
+
+puppeteer.use(StealthPlugin());
 
 const allRequestsTypes = [
   "stylesheet",
@@ -16,19 +19,14 @@ const allRequestsTypes = [
 ];
 
 const setupBrowserPage = async ({ allowedRequests }) => {
-  const browser =
-    process.env.USE_DEVTOOLS == "true"
-      ? await puppeteer.launch({ devtools: true })
-      : await puppeteer.launch({ args: ["--no-sandbox"] });
+  const browser = await puppeteer.launch({
+    args: ["--no-sandbox"],
+    headless: true,
+  });
 
   const page = await browser.newPage();
   await page.setRequestInterception(true);
-  // Pass the User-Agent Test.
-  const userAgent =
-    "Mozilla/5.0 (X11; Linux x86_64)" +
-    "AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.39 Safari/537.36";
-  await page.setUserAgent(userAgent);
-  
+
   if (process.env.DEBUG == true)
     page.setViewport({ width: "1200", height: "1000" });
 
