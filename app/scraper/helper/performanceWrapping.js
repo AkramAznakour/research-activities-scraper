94d3df30b9142fd1946b4fde84309aa0efdcc9e2
Akram Aznakour <35656299+AkramAznakour@users.noreply.github.com>
2020-08-09 01:59:56 +0100

    Add scraper helpers

---
+++ app/scraper/helper/performanceWrapping.js
@@ -0,0 +1,28 @@
+const { performance } = require("perf_hooks");
+
+const performanceWrapping = (jobFunction) => async ({
+  performanceWrapping = true,
+  ...args
+}) => {
+  if (!performanceWrapping) return await jobFunction(args);
+
+  const t0 = performance.now();
+  const result = await jobFunction(args);
+  const t1 = performance.now();
+
+  console.log("calling :", jobFunction.name);
+  console.log("args :", args);
+  console.log("took :", parseInt(t1 - t0), "ms \n");
+
+  const used = process.memoryUsage();
+  for (let key in used)
+    console.log(
+      `Memory: ${key} ${Math.round((used[key] / 1024 / 1024) * 100) / 100} MB`
+    );
+
+  console.log("------------------------------------");
+
+  return result;
+};
+
+exports.performanceWrapping = performanceWrapping;
---
+++ app/scraper/helper/setupBrowserPage.js
@@ -0,0 +1,40 @@
+const puppeteer = require("puppeteer");
+
+const allRequestsTypes = [
+  "stylesheet",
+  "image",
+  "media",
+  "font",
+  "script",
+  "texttrack",
+  "xhr",
+  "fetch",
+  "eventsource",
+  "websocket",
+  "manifest",
+  "other",
+];
+
+const setupBrowserPage = async ({ allowedRequests }) => {
+  const browser =
+    process.env.USE_DEVTOOLS == "true"
+      ? await puppeteer.launch({ devtools: true })
+      : await puppeteer.launch({ args: ["--no-sandbox"] });
+
+  const page = await browser.newPage();
+  await page.setRequestInterception(true);
+
+  const forbiddenRequestsTypes = allRequestsTypes.filter(
+    (requestType) => !allowedRequests.includes(requestType)
+  );
+
+  page.on("request", (request) =>
+    forbiddenRequestsTypes.includes(request.resourceType())
+      ? request.abort()
+      : request.continue()
+  );
+
+  return { browser, page };
+};
+
+exports.setupBrowserPage = setupBrowserPage;
