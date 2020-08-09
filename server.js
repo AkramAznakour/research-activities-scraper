5d0c39c88d3387e2d3c4b494b6d94c92f0791f96
Akram Aznakour <35656299+AkramAznakour@users.noreply.github.com>
2020-08-09 01:59:11 +0100

    Rename app to server and split routes

---
+++ server.js
@@ -0,0 +1,16 @@
+const dotenv = require("dotenv");
+dotenv.config();
+
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.listen(process.env.PORT || 2000, () =>
+  console.log("Server started on port :", process.env.PORT || 2000)
+);
+
+const router = require("./routes");
+app.use("/", router);
