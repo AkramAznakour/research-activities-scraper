d160afaa03e6dfa7569021e5757ba0ff435c8388
Akram Aznakour <35656299+akramaznakour@users.noreply.github.com>
2020-08-11 17:55:12 +0100

    Bugfix attempt to prevent google detection (#26)
    * Add --headless lunch argument
    
    * Remove user agent
    
    * Remove redundant set viewport
    
    * Attempt to prevent detection

--- app/scraper/helper/setupBrowserPage.js
+++ app/scraper/helper/setupBrowserPage.js
@@ -20,26 +20,21 @@ const allRequestsTypes = [
 
 const setupBrowserPage = async ({ allowedRequests }) => {
   const browser = await puppeteer.launch({
-    args: ["--no-sandbox"],
+    args: [ "--no-sandbox", "--headless" ],
     headless: true,
   });
 
   const page = await browser.newPage();
   await page.setRequestInterception(true);
-  // Pass the User-Agent Test.
-  const userAgent =
-    "Mozilla/5.0 (X11; Linux x86_64)" +
-    "AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.39 Safari/537.36";
-  await page.setUserAgent(userAgent);
-  
-  if (process.env.DEBUG == true)
-    page.setViewport({ width: "1200", height: "1000" });
 
   if (process.env.DEBUG == true)
     page.setViewport({ width: "1200", height: "1000" });
 
-  if (process.env.DEBUG == true)
-    page.setViewport({ width: "1200", height: "1000" });
+  await page.evaluateOnNewDocument(() => {
+    const newProto = navigator.__proto__;
+    delete newProto.webdriver;
+    navigator.__proto__ = newProto;
+  });
 
   const forbiddenRequestsTypes = allRequestsTypes.filter(
     (requestType) => !allowedRequests.includes(requestType)
