f07df3eea41862e759e7dabdd9e3c212b0c04530
Akram Aznakour <35656299+AkramAznakour@users.noreply.github.com>
2020-08-11 17:52:38 +0100

    Remove user agent

--- app/scraper/helper/setupBrowserPage.js
+++ app/scraper/helper/setupBrowserPage.js
@@ -26,12 +26,6 @@ const setupBrowserPage = async ({ allowedRequests }) => {
 
   const page = await browser.newPage();
   await page.setRequestInterception(true);
-  // Pass the User-Agent Test.
-  const userAgent =
-    "Mozilla/5.0 (X11; Linux x86_64)" +
-    "AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.39 Safari/537.36";
-  await page.setUserAgent(userAgent);
-  
   if (process.env.DEBUG == true)
     page.setViewport({ width: "1200", height: "1000" });
 
