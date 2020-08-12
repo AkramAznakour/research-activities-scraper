886256d8873013ed51d226af017f057a25e8fb0b
Akram Aznakour <35656299+AkramAznakour@users.noreply.github.com>
2020-08-12 14:58:02 +0100

    Remove express redundant declaration

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const dotenv = require("dotenv");
 dotenv.config();
 
-const express = require("express");
 const cors = require("cors");
 const app = express();
 
