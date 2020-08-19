84185c622fdec3822ec816db9d5a0dd16d67a708
Akram Aznakour <35656299+AkramAznakour@users.noreply.github.com>
2020-08-19 09:44:47 +0100

    Send calculated citations

--- app/scraper/scopusScraper.js
+++ app/scraper/scopusScraper.js
@@ -202,7 +202,7 @@ const authorData = async ({ authorId }) => {
         interests,
         publications,
         coauthors: [],
-        citationsPerYear: [],
+        citationsPerYear,
       };
     });
 
