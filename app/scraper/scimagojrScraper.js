2bb11546dd5e3b29cd896b3b477e2d6b7ef07ff2
Akram Aznakour <35656299+AkramAznakour@users.noreply.github.com>
2020-08-24 22:19:29 +0100

    Add selectors

--- app/scraper/scimagojrScraper.js
+++ app/scraper/scimagojrScraper.js
@@ -1,5 +1,12 @@
 const { performanceWrapping } = require("./helper/performanceWrapping");
 const { setupBrowserPage } = require("./helper/setupBrowserPage");
+
+const SCIMAGOJR_URL = "https://www.scimagojr.com/journalsearch.php?";
+const POSSIBLE_JOURNALS_SELECTOR =
+  "body > div.journaldescription.colblock > div.search_results > a";
+const SJR_LIST_SELECTOR =
+  "body > div.dashboard > div:nth-child(2) > div.cellcontent > div:nth-child(2) > table > tbody > tr";
+
 const DIRECT_NAVIGATION_OPTIONS = {
   waitUntil: "load",
   timeout: 0,
