ef657c495bdaee43f326bebefc6c9bfba91a2079
Akram Aznakour <35656299+AkramAznakour@users.noreply.github.com>
2020-08-24 22:16:57 +0100

    Add performanceWrapping and setupBrowserPage

--- app/scraper/scimagojrScraper.js
+++ app/scraper/scimagojrScraper.js
@@ -1,3 +1,5 @@
+const { performanceWrapping } = require("./helper/performanceWrapping");
+const { setupBrowserPage } = require("./helper/setupBrowserPage");
 const DIRECT_NAVIGATION_OPTIONS = {
   waitUntil: "load",
   timeout: 0,
