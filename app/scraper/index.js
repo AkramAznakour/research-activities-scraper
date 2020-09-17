f102d176ccbfacc5e143b99bcd4ed9c65a161889
Akram Aznakour <35656299+AkramAznakour@users.noreply.github.com>
2020-09-17 10:07:31 +0100

    Add clarivateScraper to exports

--- app/scraper/index.js
+++ app/scraper/index.js
@@ -2,10 +2,12 @@ const scholarScraper = require("./scholarScraper");
 const scopusScraper = require("./scopusScraper");
 const guideJournalScraper = require("./guideJournalScraper");
 const scimagojrScraper = require("./scimagojrScraper");
+const clarivateScraper = require("./clarivateScraper");
 
 module.exports = {
   scholarScraper,
   scopusScraper,
   guideJournalScraper,
-  scimagojrScraper
+  scimagojrScraper,
+  clarivateScraper,
 };
