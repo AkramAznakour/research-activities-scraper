3f2127f9cbaf065b74641103976ca4d4d6e8f762
Akram Aznakour <35656299+AkramAznakour@users.noreply.github.com>
2020-09-17 10:51:14 +0100

    Test puppeteer.launch in prod

--- app/scraper/helper/setupBrowserPage.js
+++ app/scraper/helper/setupBrowserPage.js
@@ -19,15 +19,11 @@ const allRequestsTypes = [
 ];
 
 const setupBrowserPage = async ({ allowedRequests, useNativePuppeteer }) => {
-  const browser = useNativePuppeteer && useNativePuppeteer === true
-      ? await nativePuppeteer.launch({
-          args: ["--no-sandbox"],
-          headless: true,
-        })
-      : await puppeteer.launch({
-          args: ["--no-sandbox"],
-          headless: true,
-        });
+  const browser = await puppeteer.launch({
+    args: ["--no-sandbox"],
+    headless: true,
+  });
+
 
   const page = await browser.newPage();
   await page.setRequestInterception(true);
